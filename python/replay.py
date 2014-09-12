b38d5623f5fba23a83c6ecbfd7353561aeecc842
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2014-09-12 19:33:30 +0400

    Update replay.py

--- python/replay.py
+++ python/replay.py
@@ -23,12 +23,17 @@ class SIGN:
 class File(object):
 
     def __init__(self, input_file):
-  
+        
+        # read from wotreplay file 2 long values (4 + 4 = 8 bytes)
+        # first long - file signature
+        # second long - count of blocks with json data
         sign, block_count = struct.unpack('ll', input_file.read(8))
+        
         self.sign = sign
         self.json_battle_initial = None
         self.json_battle_result = None
       
+        # check for valid file signature
         if self.sign == SIGN.REGULAR:
             pass
         elif self.sign == SIGN.COMP2:
@@ -36,11 +41,21 @@ class File(object):
         else:
             raise Exception("Looks like not wotreplay file, sign: %d" % self.sign)
       
+        # read next long value (4 bytes). it's length in bytes for next json string.
         block_len, = struct.unpack('l', input_file.read(4))
+        
+        # read json string of a given size and convert to python data types
+        # save as data at battle start
         self.json_battle_initial = json.loads(input_file.read(block_len))
       
+        # if file contain block with json data at battle finish (complete record, player not leaves battle)
         if block_count > 1:
+            
+            # read long value (4 bytes) as size of next json string
             block_len, = struct.unpack('l', input_file.read(4))
+            
+            # read json string of a given size and convert to python data types
+            # save as data at battle finish
             self.json_battle_result = json.loads(input_file.read(block_len))
 
 if __name__ == "__main__":
